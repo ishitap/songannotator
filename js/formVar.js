a3170c819d3608d244e43862eeba8441155e9998
Ishita Prasad <ishitap@stanford.edu>
2015-02-28 14:43:40 -0800

    Update formVar.js

--- js/formVar.js
+++ js/formVar.js
@@ -1,9 +1,11 @@
 testArray = []
+annotation = {}
 
 document.getElementById("addButton").onclick = function(){
 	formElems = document.getElementById("annForm").elements;
 	time = formElems[0].value;
 	text = formElems[1].value;
-	testArray.push([time,text]);
+	annotation.timestamp = time;
+	annotation.text = text;
 };
 
