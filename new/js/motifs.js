4b01e11b188d2e1d7f9d83b4cf9a7dd0c4c8a049
Aaron Brown <abrown94@stanford.edu>
2015-03-11 17:49:23 -0700

    Completed waveform functionality w/ annotations

---
+++ new/js/motifs.js
@@ -0,0 +1,202 @@
+//motif -> {motif text, timestamp of addition, array of corresponding annotation IDs}
+var motifs = [];
+
+//Allows user to add motif to their collection of motifs
+$("#motForm").submit(function() {
+	event.preventDefault();
+	var text = $('input[name="motText"]').val();
+	text = text.replace(/\s+/g, '');
+	var time = Date.now();
+	addMotif({mName: text, timestamp: time, ann: []}, null);
+	this.reset();
+});
+
+$("#filterForm").submit(function() {
+	event.preventDefault();
+	var text = $('input[name="filterText"]').val();
+	for (i = 0; i < motifs.length; i++){
+		if (motifs[i]["mName"] == text){
+			var anns = motifs[i]["ann"];
+			for (j = 0; j < annotations.length; j++){
+				if (anns.indexOf(annotations[j]["displayID"]) == -1){
+					$("#" + annotations[j]["displayID"]).hide();
+				}
+			}
+		}
+	}
+});
+
+function filter(motifId){
+	clearFilter();
+	for (i = 0; i < motifs.length; i++){
+		if ($('#link' + motifs[i].timestamp).attr('class') == 'clicked' && motifs[i].timestamp != motifId){
+			$('#link' + motifs[i].timestamp).toggleClass('clicked').toggleClass('unclicked');
+		}
+	}
+	if ($('#link' + motifId).attr('class') == 'unclicked'){
+		for (i = 0; i < motifs.length; i++){
+			if (motifs[i]["timestamp"] == motifId){
+				var anns = motifs[i]["ann"];
+				console.log("ANNS", anns);
+				if (anns.length == 0){
+					clearFilter();
+					$('#link' + motifId).toggleClass('clicked').toggleClass('unclicked');
+					$('#filterText').show();
+					return;
+				}
+				for (j = 0; j < annotations.length; j++){
+					if (anns.indexOf(annotations[j]["displayID"]) == -1){
+						console.log(annotations[j]["displayID"]);
+						$("#" + annotations[j]["displayID"]).hide();
+						annotations[j].tick.visible = false;
+					}
+				}
+				view.draw();
+				var showText = "Showing " + anns.length + " of " + annotations.length + " annotations";
+				$('#filterText').text(showText);
+				$('#filterText').show();
+				break;
+			}
+		}
+	}
+	else{
+		clearFilter();
+	}	
+	$('#link' + motifId).toggleClass('clicked').toggleClass('unclicked');
+}
+
+function clearFilter(){
+	for (j = 0; j < annotations.length; j++){
+		$("#" + annotations[j]["displayID"]).show();
+		annotations[j].tick.visible = true;
+	}
+	view.draw();
+	$('#filterText').text("No Annotations Match this Tag");
+	$('#filterText').hide();
+}
+
+// Helper Function: Finds the right spot for the motif to be added within an alphabetically sorted array
+function findMIndex(motif) {
+	var index = motifs.length;
+	for (i = 0; i < motifs.length; i++){
+		if (motifs[i]["mName"] > motif.mName){
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
+
+//Adds motif directly as an annotation
+function addClickFunction(motifId){
+	var text = "";
+	for (i = 0; i < motifs.length; i++){
+		if (motifs[i].timestamp == motifId){
+			text = "#" + motifs[i].mName;
+			break;
+		}
+	}
+	var timing = Math.floor($("audio")[0].currentTime);
+	var ann = {timestamp: timing, text: text};
+	displayAnnotation(ann);
+}
+
+
+//Function to delete a motif
+function deleteFunction(motifId){
+	var mot = 0;
+	var motText = "";
+	for (i = 0; i < motifs.length; i++){
+		if (motifs[i]["timestamp"] == motifId){
+			mot = i;
+			motText = motifs[i]["mName"];
+			break;
+		}
+	}
+	motifs.splice(mot,1);
+	for (i = 0; i < annotations.length; i++){
+		if (annotations[i]["motifs"]){
+			var aMot = annotations[i]["motifs"].indexOf(motifId);
+			if (aMot > -1){
+				annotations[i]["motifs"].splice(aMot,1);
+				var val = annotations[i]["text"];
+				var mVal = val.indexOf("#" + motText);
+				var firstPart = val.substring(0,mVal);
+				var secondPart = val.substring(mVal+motText.length+2,val.length);
+				annotations[i]["text"] = firstPart.concat(secondPart);
+				valId = "#ann" + annotations[i]["displayID"];
+				$(valId)[0].innerHTML = annotations[i]["text"];
+			}
+		}
+	}
+	$("#" + motifId).remove();
+}
+
+//Adds Motif to the Table Display
+function addMotif(motif, annotation) {
+	for (i = 0; i < motifs.length; i++){
+		if (motifs[i].mName == motif.mName){
+			if (annotation){
+				motifs[i].ann.push(annotation.displayID);
+				if (annotation["motifs"]){
+					annotation["motifs"].push(motifs[i].timestamp);
+				}
+				else{
+					annotation["motifs"] = [motifs[i].timestamp];
+				}
+			}
+			return;
+		}
+	}
+
+	if(annotation){
+		motif.ann.push(annotation.displayID);
+		if (annotation["motifs"]){
+			annotation["motifs"].push(motif.timestamp);
+		}
+		else{
+			annotation["motifs"] = [motif.timestamp];
+		}
+	}
+
+	motifId = motif.timestamp;	
+
+	var newElem = "<ul id='" + motifId + "'><a href='#' id='link"+ motifId +"'class='unclicked' onclick='filter("+ motifId + ")'>"+ motif.mName + "  " + "</a><span class='glyphicon glyphicon-remove remove-motif' onclick='deleteFunction("+motifId+")' aria-hidden='true'></span></ul>";
+
+	$('#' + motifId).mouseover(function(){
+		console.log("hello");
+	})
+
+	var mIndex = findMIndex(motif);
+	var prevMIndex = mIndex - 1;
+
+	if (prevMIndex >= 0) {
+		// console.log(motif.mName, prevMIndex, motifs[prevMIndex].mName, motifs[prevMIndex].timestamp)
+		var prev = $('#motif-list').find('#' + motifs[prevMIndex].timestamp);
+		prev.after(newElem);
+	}
+	else{ 
+		$('#motif-list').find('#first-elem').after(newElem);
+	}
+
+	if (mIndex >= motifs.length)
+		motifs.push(motif);
+	else {
+		motifs.splice(mIndex, 0, motif);
+	}
+}
+
+function addInitialMotifs(){
+	time = Date.now();
+	motif1 = {mName:"formation-change", timestamp:time, ann:[]};
+	addMotif(motif1, null);
+	motif2 = {mName:"smooth-music", timestamp:time+1, ann:[]};
+	addMotif(motif2, null);
+	motif3 = {mName:"storyline", timestamp:time+2, ann:[]};
+	addMotif(motif3, null);
+	motif5 = {mName:"music-annotation", timestamp:time+4, ann:[]};
+	addMotif(motif5,null);
+	motif6 = {mName:"choreo-annotation", timestamp:time+5, ann:[]};
+	addMotif(motif6, null);
+}
+
---
+++ new/js/waveform.js
@@ -0,0 +1,139 @@
+/* Waveform intialization */
+
+// Create an instance
+var wavesurfer = Object.create(WaveSurfer);
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function () {
+  // Set options
+  var options = {
+    container: document.querySelector('#waveform'),
+    loaderColor: '#DDDDDD',
+    waveColor: '#7FC6BC',
+    progressColor: '#4BB5C1',
+    cursorColor: '#777777',
+    height: 150,
+    scrollParent: false
+  };
+
+  // Init
+  wavesurfer.init(options);
+  // Load audio from URL
+  wavesurfer.load('feelsoclose.mp3');
+});
+
+// Report errors
+wavesurfer.on('error', function (err) {
+  console.error(err);
+});
+
+// Progress bar
+document.addEventListener('DOMContentLoaded', function () {
+  var progressDiv = document.querySelector('#progress-bar');
+  var progressBar = progressDiv.querySelector('.progress-bar');
+
+  var showProgress = function (percent) {
+    progressDiv.style.display = 'block';
+    progressBar.style.width = percent + '%';
+  };
+
+  var hideProgress = function () {
+    progressDiv.style.display = 'none';
+  };
+
+  wavesurfer.on('loading', showProgress);
+  wavesurfer.on('ready', hideProgress);
+  wavesurfer.on('destroy', hideProgress);
+  wavesurfer.on('error', hideProgress);
+});
+
+/* Load controls */
+
+var GLOBAL_ACTIONS = {
+    'play': function () {
+        wavesurfer.playPause();
+        // Change icon
+        var $playPause = $("#play-pause");
+        if ($playPause.hasClass("glyphicon-play")) {
+            $playPause.removeClass("glyphicon-play");
+            $playPause.addClass("glyphicon-pause");
+        } else if ($playPause.hasClass("glyphicon-pause")) {
+            $playPause.removeClass("glyphicon-pause");
+            $playPause.addClass("glyphicon-play");
+        }
+    },
+
+    'back': function () {
+        wavesurfer.skipBackward();
+    },
+
+    'forth': function () {
+        wavesurfer.skipForward();
+    },
+
+    'mute': function () {
+        wavesurfer.toggleMute();
+        // Change icon
+        var $volume = $("#volume");
+        if ($volume.hasClass("glyphicon-volume-up")) {
+            $volume.removeClass("glyphicon-volume-up");
+            $volume.addClass("glyphicon-volume-off");
+        } else if ($volume.hasClass("glyphicon-volume-off")) {
+            $volume.removeClass("glyphicon-volume-off");
+            $volume.addClass("glyphicon-volume-up");
+        }
+    }
+};
+
+// Bind actions to buttons and keypresses
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('keydown', function (e) {
+        var map = {
+            // 32: 'play',       // space
+            37: 'back',       // left
+            39: 'forth'       // right
+        };
+        var action = map[e.keyCode];
+        if (action in GLOBAL_ACTIONS) {
+            if (document == e.target || document.body == e.target) {
+                e.preventDefault();
+            }
+            GLOBAL_ACTIONS[action](e);
+        }
+    });
+
+    [].forEach.call(document.querySelectorAll('[data-action]'), function (el) {
+        el.addEventListener('click', function (e) {
+            var action = e.currentTarget.dataset.action;
+            if (action in GLOBAL_ACTIONS) {
+                e.preventDefault();
+                GLOBAL_ACTIONS[action](e);
+            }
+        });
+    });
+});
+
+/* Callback */
+wavesurfer.on('ready', function () {
+  // Check current time to highlight annotations
+  window.setInterval(function() {
+    var seconds = Math.floor(wavesurfer.getCurrentTime());
+    highlight(seconds);
+    document.getElementById("time").innerHTML = formatTimestamp(seconds);
+  }, 100);
+
+  console.log("Loaded wavesurfer");
+
+  annotationList = $('#annotation-list');
+  annotationTemplate = Handlebars.compile($("#annotation-template").html());
+  canvas = document.getElementById("timeline-canvas");
+
+  ann = [ { timestamp: 1, text: "First annotation"},
+              { timestamp: 4, text: "Second annotation"},
+              { timestamp: 12, text: "Opportunity for ramp-up"},
+              { timestamp: 30, text: "Bass drop"}]
+
+  setupPaper();
+  displayAllAnnotations(ann);
+  addInitialMotifs();
+});
